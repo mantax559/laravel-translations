b877c936446501e3e296b95c272d4be640178c72
Mantasblo <mantax559@gmail.com>
2024-07-02 17:39:18 +0300

    Bugfix

--- src/Helpers/TranslationHelper.php
+++ src/Helpers/TranslationHelper.php
@@ -9,25 +9,31 @@ class TranslationHelper
 {
     public static function getValidationRules(string $translationName, bool $withDescription = false, bool $withTranslationStatus = false): array
     {
-        $rules = [
-            $translationName => ValidationHelper::getArrayRules(),
-        ];
+        $rules = [];
 
-        foreach (config('laravel-translations.locales') as $index => $locale) {
-            if (cmprint($index, 0)) {
-                $rules["$translationName.$locale"] = ValidationHelper::getArrayRules();
-            }
+        foreach (config('laravel-translations.locales') as $locale) {
+            $rules = [];
+            $rulesCount = 1;
 
             if ($withTranslationStatus) {
                 $rules["$translationName.$locale.translation_status"] = ValidationHelper::getEnumRules(enum: TranslationStatusEnum::class);
+                $rulesCount++;
             }
 
             if ($withDescription) {
                 $rules["$translationName.$locale.title"] = ValidationHelper::getStringRules(required: "required_with:$locale.description");
                 $rules["$translationName.$locale.description"] = ValidationHelper::getTextRules(required: false);
+                $rulesCount++;
             } else {
                 $rules["$translationName.$locale.title"] = ValidationHelper::getStringRules(required: false);
             }
+
+            $rules["$translationName.$locale"] = ValidationHelper::getArrayRules(min: $rulesCount, max: $rulesCount);
+        }
+
+        if (! empty($rules)) {
+            $rulesCount = count($rules);
+            $rules[$translationName] = ValidationHelper::getArrayRules(min: $rulesCount, max: $rulesCount); // TODO: Add "size" to getArrayRules
         }
 
         return $rules;
@@ -67,8 +73,10 @@ class TranslationHelper
             TranslationStatusEnum::Auto,
         ];
 
+        $modelTranslations = $model->translations;
         foreach ($translationStatuses as $status) {
-            $translation = $model->translations->where('translation_status', $status)->first();
+            $translation = $modelTranslations->where('translation_status', $status)->first();
+
             if ($translation) {
                 return $translation;
             }
